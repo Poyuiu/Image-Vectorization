64e666c13e06fc579c14574867a2915dd83c870d
zxas97050 <zxas97050@gmail.com>
2023-06-16 01:03:43 +0800

    重跑128 svg

--- src/mse_func.py
+++ src/mse_func.py
@@ -3,11 +3,23 @@ from seam_carving import resize_image
 
 
 def calculate_mse(original, result):
-    if original.shape != result.shape:
-        # using seam carving to handle different ratios
-        if original.shape[0] > result.shape[0]:
-            original = resize_image(original, result.shape[0], result.shape[1])
-        else:
-            result = resize_image(result, original.shape[0], original.shape[1])
+    M, N, C = original.shape
+    temp = np.zeros((M, N, 3))
+    if C == 4:
+        for i in range(M):
+            for j in range(N):
+                if original[i, j, 3] == 0:
+                    temp[i, j] = np.array([255, 255, 255])
+                else:
+                    temp[i, j] = original[i, j, :3]
+    else:
+        temp = original
 
-    return ((original.astype(np.float64) - result.astype(np.float64)) ** 2).mean()
+    if temp.shape != result.shape:
+        # using seam carving to handle different ratios
+        if temp.shape[0] > result.shape[0]:
+            temp = resize_image(temp, result.shape[0], result.shape[1])
+        else:
+            result = resize_image(result, temp.shape[0], temp.shape[1])
+
+    return ((temp.astype(np.float64) - result.astype(np.float64)) ** 2).mean()
--- src/test.py
+++ src/test.py
@@ -6,33 +6,44 @@ import k_means
 import png2svg_func
 import svg2png_func
 import mse_func
-import random
 
 # cd src
-directory = r"C:\Users\user\Desktop\me\NTHU\32 multimedia\Final Project\png\128"
-store_dir = r"C:\Users\user\Desktop\me\NTHU\32 multimedia\Final Project\out_svg"
+directory = 'C:/Users/user/Documents/GitHub/Image-Vectorization/png/128'
+store_dir = 'C:/Users/user/Documents/GitHub/Image-Vectorization/out_svg'
 
-total = 0
 # file_num = len(os.listdir(directory))  # 3458
+total = 0
 cnt = 0
-all_files = os.listdir(directory)
 
-random.shuffle(all_files)
+for filename in os.listdir(directory):
+    original = cv2.imread(directory + '/' + filename, cv2.IMREAD_UNCHANGED)
 
-for filename in all_files:
-    original = cv2.imread(directory + "\\" + filename)
+    M, N, C = original.shape
+    transparent = np.zeros((M, N))
+    if C == 4:
+        for i in range(M):
+            for j in range(N):
+                if original[i, j, 3] == 0:
+                    transparent[i, j] = 1
+
     img = cv2.cvtColor(original, cv2.COLOR_BGR2RGB)
     img = k_means.coverting_img(img, 32)
     img = png2svg_func.smooth_detail(img)
 
+    if C == 4:
+        for i in range(M):
+            for j in range(N):
+                if transparent[i, j]:
+                    img[i, j] = np.array([255, 255, 255])
+
     img = cv2.cvtColor(img.astype(np.uint8), cv2.COLOR_RGB2BGR)
     svg_image = png2svg_func.png2svg(img, 1)
 
-    new_filename = filename[:-3]
-    with open(store_dir + "\\" + new_filename + "svg", "w") as fh:
+    new_filename = filename[:-4]
+    with open(f'{store_dir}/{new_filename}.svg', 'w') as fh:
         fh.write(svg_image)
 
-    svg2png_func.svg2png(store_dir + "\\" + new_filename + "svg")
+    svg2png_func.svg2png(f'{store_dir}/{new_filename}.svg')
     result = cv2.imread("result.png")
     MSE = mse_func.calculate_mse(original, result)  # BGR
     total += MSE
